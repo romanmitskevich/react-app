a13a6c5628145b471e4eb33ea01a6f1c7937dc50
roman.mits <roman.mitskevich1@mail.ru>
2022-12-11 15:32:29 +0300

    feat: add days display and months switch

---
+++ src/components/CalendarCell.js
@@ -0,0 +1,28 @@
+import moment from "moment"; 
+
+const CalendarCell = ({ dayItem, index, day }) => {
+    const names = ["Понедельник", "Вторник", "Среда", "Четверг", "Пятница", "Суббота", "Воскресенье"];
+    const isToday = moment().isSame(dayItem, "day") ? "calendar-cell today" : "calendar-cell";
+    const isCurrent = dayItem.isSame(day, "month") ? true : false;
+
+    const check = () => {
+        return ((dayItem.day() === 0 || dayItem.day() === 6)
+            ? isCurrent ? <span className="cell-day red">{dayItem.format("D")}</span> : <span className="cell-day">{dayItem.format("D")}</span>
+            : isCurrent ? <span className="cell-day current">{dayItem.format("D")}</span> : <span className="cell-day">{dayItem.format("D")}</span>);
+    }
+
+    return (
+        <div className={isToday}>
+            {index < 7
+                ? isCurrent ? <span className="day-name current">{names[index]}, {check()}</span> : <span className="day-name">{names[index]}, {check()}</span>
+                : check()}
+            {/* {index < 7 ? <span className="day-name">{`${dayItem.day(index + 1).format("dddd")[0].toUpperCase()}${dayItem.day(index + 1).format("dddd").slice(1)}`}, {check()}</span> : check()} */}
+            <div className="calendar-event">
+                {isCurrent ? <h3 className="event-title current">Meet</h3> : <h3 className="event-title">Meet</h3>}
+                {isCurrent ? <p className="event-text current">asdgas </p> : <p className="event-text">asdgas </p>}
+            </div>
+        </div>
+    )
+}
+
+export default CalendarCell;
--- src/pages/Calendar.js
+++ src/pages/Calendar.js
@@ -1,4 +1,20 @@
+import moment from "moment/moment";
+import "moment/locale/ru";
+import { useState } from "react";
+import CalendarCell from "../components/CalendarCell";
+
 const Calendar = () => {
+    moment.updateLocale("ru", { week: { dow: 1 } });
+    const [day, setCurrentCalendar] = useState(moment());
+    const firstDayToRender = day.clone().startOf('month').startOf('week');
+    const firstDay = firstDayToRender.subtract(1, "day");
+
+    const daysArray = [...Array(42)].map(() => firstDay.add(1, "day").clone());
+
+    const previousHandler = () => setCurrentCalendar(prev => prev.clone().subtract(1, "month"));
+    const nextHandler = () => setCurrentCalendar(next => next.clone().add(1, "month"));
+    const todayHandler = () => setCurrentCalendar(moment());
+
     return (
         <div className="main-container">
             <div className="calendar-container">
@@ -13,12 +29,14 @@ const Calendar = () => {
                     </div>
                 </div>
                 <div className="calendar-date">
-                    <button className="previous-month"></button>
-                    <div className="current-date">Месяц Год</div>
-                    <button className="next-month"></button>
-                    <button className="current-day">Сегодня</button>
+                    <button className="previous-month" onClick={previousHandler}></button>
+                    <div className="current-date">{`${day.format("MMMM")[0].toUpperCase()}${day.format("MMMM").slice(1)}`} {day.year()}</div>
+                    <button className="next-month" onClick={nextHandler}></button>
+                    <button className="current-day" onClick={todayHandler}>Сегодня</button>
                 </div>
-                <div className="calendar-table"></div>
+                <div className="calendar-table">
+                    {daysArray.map((dayItem, index) => <CalendarCell key={dayItem.unix()} dayItem={dayItem} index={index} day={day} />)}
+                </div>
             </div>
         </div>
     )
