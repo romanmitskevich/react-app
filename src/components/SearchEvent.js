f20d5a2e6f05209b809ac6a077858d35d382e0ab
roman.mits <roman.mitskevich1@mail.ru>
2022-12-12 14:30:44 +0300

    fix: change event search

--- src/components/SearchEvent.js
+++ src/components/SearchEvent.js
@@ -2,10 +2,11 @@ const SearchEvent = ({ active, setActive, event }) => {
     return (
         <div className={active ? "search-event-window active" : "search-event-window"} onClick={() => setActive(false)}>
             <div className={active ? "search-window active" : "search-window"} onClick={e => e.stopPropagation()}>
-                <h3>Найденное событие</h3>
-                <p><span>Дата: </span>{event.date}</p>
-                <p><span>Тема: </span>{event.title}</p>
-                <p><span>Описание: </span>{event.text}</p>
+                {event ? <div><h3>Найденное событие</h3>
+                    <p><span>Дата: </span>{event.date}</p>
+                    <p><span>Тема: </span>{event.title}</p>
+                    <p><span>Описание: </span>{event.text}</p></div> : <h3>Событие не найдено</h3>}
+
             </div>
         </div>
     )
